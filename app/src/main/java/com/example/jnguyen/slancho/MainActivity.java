dcae62fc482bc920aeb8627c4a25225450d1bf5d
johnnyn95 <johnnyn@abv.bg>
2018-01-30 18:22:32 +0200

    Nothing works,a lot of things to implement

--- app/src/main/java/com/example/jnguyen/slancho/MainActivity.java
+++ app/src/main/java/com/example/jnguyen/slancho/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.jnguyen.slancho;
 
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.jnguyen.slancho.Utilities.NetworkUtilities;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        ForecastAdapter.ForecastAdapterOnClickHandler{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ForecastAdapter mForecastAdapter;
+
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerview_forecast);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mForecastAdapter = new ForecastAdapter(this,this);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mForecastAdapter);
+
+        //TODO test network utilities
+        TextView test = (TextView) findViewById(R.id.tv_test);
+        URL testUrl = NetworkUtilities.buildURLforFiveDays("Sofia");
+
+
+    }
+
+    @Override
+    public void onClick(long date) {
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
