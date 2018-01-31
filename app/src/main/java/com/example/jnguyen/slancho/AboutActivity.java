de96e481968fb3bc7571fa2ada1ab624f1e023d5
johnnyn95 <johnnyn@abv.bg>
2018-01-31 18:02:00 +0200

    Added menu,Settings Activity(not implemented),Share(partially implemented),and About.

---
+++ app/src/main/java/com/example/jnguyen/slancho/AboutActivity.java
@@ -0,0 +1,32 @@
+package com.example.jnguyen.slancho;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TextView mDescription = findViewById(R.id.about_description);
+        String aboutDescription = getResources().getString( R.string.about_description);
+        mDescription.setText(aboutDescription);
+    }
+
+    private  void openLinkedInProfile(View v){
+        String linkedInLink = getResources().getString( R.string.linkedInLink);
+        Uri uri = Uri.parse(linkedInLink);
+        Intent webpageIntent = new Intent(Intent.ACTION_VIEW,uri);
+
+        if (webpageIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(webpageIntent);
+        }
+    }
+}
--- app/src/main/java/com/example/jnguyen/slancho/MainActivity.java
+++ app/src/main/java/com/example/jnguyen/slancho/MainActivity.java
@@ -1,46 +1,73 @@
 package com.example.jnguyen.slancho;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.jnguyen.slancho.Utilities.NetworkUtilities;
 
 import java.net.URL;
+//TODO Implement Settings/Preferences
+//TODO Implement Share Function
+//TODO Implement About
+//TODO Create and Implement Adapter
+//TODO Create Content Provider
 
 public class MainActivity extends AppCompatActivity implements
-        LoaderManager.LoaderCallbacks<Cursor>,
-        ForecastAdapter.ForecastAdapterOnClickHandler{
+        LoaderManager.LoaderCallbacks<Cursor>{
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemClickedId = item.getItemId();
+        switch (itemClickedId){
+            case R.id.action_settings :
+                Intent settingsIntent = new Intent(this,SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            case R.id.action_share :
+                //TODO Implement/Decide what type of data to share
+                String mimeType = "text/plain";
+                String title = "Weather shared from Slancho App!";
+                String textToShare = "Today the weather will be Sunny, MAX TEMP:10 MIN TEMP:3";
+
+                ShareCompat.IntentBuilder.from(this)
+                        .setType(mimeType)
+                        .setChooserTitle(title)
+                        .setText(textToShare)
+                        .startChooser();
+                return true;
+            case R.id.action_about :
+                Intent aboutIntent = new Intent(this,AboutActivity.class);
+                startActivity(aboutIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ForecastAdapter mForecastAdapter;
-
-        RecyclerView mRecyclerView = findViewById(R.id.recyclerview_forecast);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        mForecastAdapter = new ForecastAdapter(this,this);
-
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setAdapter(mForecastAdapter);
 
         //TODO test network utilities
         TextView test = (TextView) findViewById(R.id.tv_test);
         URL testUrl = NetworkUtilities.buildURLforFiveDays("Sofia");
 
-
-    }
-
-    @Override
-    public void onClick(long date) {
 
     }
 
